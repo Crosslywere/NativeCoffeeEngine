b4d0ff672b1400404420f203cd53a6c22940e6ed
Crosslywere <ogboruj@outlook.com>
2026-02-10 09:04:39 +0100

    Used glad to load OpenGL in Test native method

--- src/main/native/CoffeeEngineImpl.cpp
+++ src/main/native/CoffeeEngineImpl.cpp
@@ -1,17 +1,9 @@
-#include "io_coffee_engine_CoffeeEngine.h"
+#include <io_coffee_engine_CoffeeEngine.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <chrono>
 #include <cmath>
 #include <iostream>
-
-inline const std::string utos(unsigned int i) {
-    char num = '0' + abs(i % 10);
-    const char* pnum = &num;
-    if (i >= 10) {
-        return utos(i / 10) + std::string(pnum);
-    }
-    return std::string(pnum);
-}
 
 JNIEXPORT jboolean JNICALL Java_io_coffee_1engine_CoffeeEngine_Test(JNIEnv *env, jclass clazz)
 {
@@ -19,13 +11,26 @@ JNIEXPORT jboolean JNICALL Java_io_coffee_1engine_CoffeeEngine_Test(JNIEnv *env,
         std::cout << "Test failed on init" << std::endl;
         return false;
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     std::string title = "Test Window will close in ";
-    GLFWwindow* window = glfwCreateWindow(800, 600, (title + "5s").c_str(), nullptr, nullptr);
+    char timeBuffer[5];
+    std::sprintf(timeBuffer, "%.1fs", 5.0f);
+    GLFWwindow* window = glfwCreateWindow(800, 600, (title + timeBuffer).c_str(), nullptr, nullptr);
     if (!window) {
         std::cout << "Test failed on window creation"  << std::endl;
         glfwTerminate();
         return false;
     }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Test failed on loading OpenGL" << std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return false;
+    }
+    std::cout << glGetString(GL_VERSION) << std::endl;
     const auto start = std::chrono::steady_clock::now();
     while(!glfwWindowShouldClose(window)) {
         const std::chrono::duration<double> duration = std::chrono::steady_clock::now() - start;
@@ -33,8 +38,11 @@ JNIEXPORT jboolean JNICALL Java_io_coffee_1engine_CoffeeEngine_Test(JNIEnv *env,
         if (sec >= 5) {
             glfwSetWindowShouldClose(window, true);
         } else {
-            glfwSetWindowTitle(window, (title + utos(5 - sec)).c_str());
+            sprintf(timeBuffer, "%.1fs", 5 - sec);
+            glfwSetWindowTitle(window, (title + timeBuffer).c_str());
         }
+        glClearColor(std::sin(sec), std::cos(sec), 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
