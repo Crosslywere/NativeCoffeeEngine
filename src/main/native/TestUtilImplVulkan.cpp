7f1e5cdeecabfa4c44a9c5c550079c3acf4004fe
Crosslywere <ogboruj@outlook.com>
2026-02-17 13:17:15 +0100

    Added window creation code

--- src/main/native/TestUtilImplVulkan.cpp
+++ src/main/native/TestUtilImplVulkan.cpp
@@ -1,28 +1,55 @@
 #include <io_coffee_engine_test_TestUtil.h>
 #include <stdexcept>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.hpp>
+#define TOTAL_RUNTIME 5.0
+
 class VulkanApp
 {
 public:
     void run()
     {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    void initWindow()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(800, 600, "Testing Vulkan (5s)", nullptr, nullptr);
+    }
+
     void initVulkan()
     {
     }
 
     void mainLoop()
     {
+        while (!glfwWindowShouldClose(window))
+        {
+            if (glfwGetTime() >= TOTAL_RUNTIME)
+            {
+                glfwSetWindowShouldClose(window, true);
+            }
+            glfwPollEvents();
+        }
     }
 
     void cleanup()
     {
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
+
+private:
+    GLFWwindow *window;
 };
 
 JNIEXPORT jboolean JNICALL Java_io_coffee_1engine_test_TestUtil_TestVulkan(JNIEnv *env, jclass)
