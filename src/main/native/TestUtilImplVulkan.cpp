d68d7e6b9018abe3223da1d3727c2ed6679338e9
Crosslywere <ogboruj@outlook.com>
2026-02-18 21:29:38 +0100

    Fixed apple support in Vulkan implementation

--- src/main/native/TestUtilImplVulkan.cpp
+++ src/main/native/TestUtilImplVulkan.cpp
@@ -71,6 +71,7 @@ private:
             requiredExtensions.emplace_back(glfwExtensions[i]);
         }
         requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
         createInfo.enabledExtensionCount = (uint32_t)requiredExtensions.size();
         createInfo.ppEnabledExtensionNames = requiredExtensions.data();
 #else
